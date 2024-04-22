2c23d5524da00f12428043dc1fe77aab4a201c2a
ParkinsonThomas <u05tp21@abdn.ac.uk>
2024-04-22 13:07:59 +0100

    TOPIC section completed - deleted some comments.

--- server/ChatServerHandler.java
+++ server/ChatServerHandler.java
@@ -102,19 +102,6 @@ public class ChatServerHandler implements Runnable {
 	}
 
 	/**
-	 * Checks message for any Topic keywords
-	 * 
-	private Boolean processMessage(String message, ConnectionPool actualPool) {
-		for (Topic topic : actualPool.getTopics()) {
-			if (message.contains(topic.getTopicName())) {
-					return true;
-				}
-			}
-			return false;
-		}
-	*/
-
-	/**
 	 * Sends a response to the client
      */
 	public void sendResponse(Response response) {
